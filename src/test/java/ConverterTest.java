fb9d0442e3563f10d45ceb042f6aef8fe8689222
Arouna Konake <119404938+ArounaK9@users.noreply.github.com>
2024-07-01 00:06:34 +0200

    Test échouant pour convertir 1 en I

--- src/test/java/ConverterTest.java
+++ src/test/java/ConverterTest.java
@@ -1,4 +1,4 @@
-// src/test/java/ConverterTest.java
+
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,19 +10,4 @@ public class ConverterTest {
         String result = converter.convert(1);
         assertThat(result).isEqualTo("I");
     }
-
-    @Test
-    public void convert_5_to_V() {
-        Converter converter = new Converter();
-        String result = converter.convert(5);
-        assertThat(result).isEqualTo("V");
-    }
-
-    @Test
-    public void convert_10_to_X() {
-        Converter converter = new Converter();
-        String result = converter.convert(10);
-        assertThat(result).isEqualTo("X");
-    }
-
 }
