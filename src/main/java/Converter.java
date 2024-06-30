12943b6743ffb78fac9f614fd019c3b51339becc
Arouna Konake <119404938+ArounaK9@users.noreply.github.com>
2024-07-01 00:07:43 +0200

    Refactoriser pour préparer la conversion de 5 en V

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -3,6 +3,8 @@ public class Converter {
     public String convert(int number) {
         if (number == 1) {
             return "I";
+        } else if (number == 5) {
+            return "V";
         }
         return "";
     }
