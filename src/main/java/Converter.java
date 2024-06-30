eb35069242bbf93b0dfa70074e5379d14790602d
Arouna Konake <119404938+ArounaK9@users.noreply.github.com>
2024-07-01 00:13:58 +0200

    Refactor: Generalize convert method to handle multiple values

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -1,13 +1,17 @@
 public class Converter {
 
     public String convert(int number) {
-        if (number == 1) {
-            return "I";
-        } else if (number == 5) {
-            return "V";
-        } else if (number == 10) {
-            return "X";
+        StringBuilder result = new StringBuilder();
+        int[] values = {10, 5, 1};
+        String[] numerals = {"X", "V", "I"};
+
+        for (int i = 0; i < values.length; i++) {
+            while (number >= values[i]) {
+                number -= values[i];
+                result.append(numerals[i]);
+            }
         }
-        return "";
+
+        return result.toString();
     }
 }
